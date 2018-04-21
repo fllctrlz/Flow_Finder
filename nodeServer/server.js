b8eb54d6eae8e5353f860aa0f3dc6cbd2a728f79
Alex-Alexiev <aalexiev@bayviewglen.ca>
2018-04-21 03:37:24 +0000

    initial commit

---
+++ nodeServer/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const path = require('path');
+const port = 3000;
+const htmlPath = path.join(__dirname, 'index.html');
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+//app.use(express.static(htmlPath)) test
+
+app.get('/magnet', (request, response) =>{
+	response.send('Fridge magnet not ready yet =(')
+});
+
+app.get('*', (request, response) => {
+	const connection = mysql.createConnection({
+		host : 'localhost',
+		user : 'main',
+		password : 'ctrlzfll',
+		database : 'flowFinder',
+		multipleStatements: true
+	});
+	connection.connect();
+	connection.query("create temporary table temp as (select userInfo.name, showerData.litres from userInfo inner join showerData on userInfo.id=showerData.id); select name, sum(litres) as totalLitres from temp group by name;", function(err, result){
+		if (err) throw err;
+		console.log(result[1]);
+		response.render('index.ejs', {data: result[1]});
+	});
+});
+
+app.listen(port, (err) => {	
+	if (err) {
+		return console.log('something went wrong');
+	}
+	console.log('server is listening on '+port);
+});
+
+
+
---
+++ serialRead/serialRead.py
@@ -0,0 +1,31 @@
+import serial
+import MySQLdb
+
+conn = MySQLdb.connect(host= "localhost", user= "main", passwd= "ctrlzfll", db= "flowFinder")
+cursor = conn.cursor()
+
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
+prevId = -1
+totalWaterUsed = 0
+
+while True:
+    line = ser.readline().decode("utf-8").split()
+    currId = int(line[0])
+    amount = int(line[1])
+    
+    if currId > -1 and currId == prevId:
+        totalWaterUsed+=amount
+    
+    if currId == -1 and prevId > -1:
+        id = prevId+1
+        litres = totalWaterUsed/1000
+        try:
+            cursor.execute("""insert into showerData (id, litres) values(%s, %s)""",(id, litres))
+            conn.commit()
+        except:
+            conn.rollback()
+        #write to database
+        totalWaterUsed = 0
+    
+    prevId = currId
